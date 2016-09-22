3fd43938f97a602ed31b03a9df3047e8a9abceba
0fp <ich@altlast.tk>
2016-09-22 17:54:29 +0000

    add output for gate

--- analoguesync.py
+++ analoguesync.py
@@ -5,23 +5,40 @@ import RPi.GPIO as GPIO
 import time
 
 def main():
-    channel = 22
+    ichannel = 22
+    ochannel = 27
     poll_freq = 500.
+    pulsewidth = 10 / 1000.
 
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(ichannel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(ochannel, GPIO.OUT)
 
     lastState = 0
-    t = 0
+    t = time.time()
+    pulseperiod = 0
     while True:
         GPIO
-        state = GPIO.input(channel)
+        state = GPIO.input(ichannel)
+
         if state != lastState:
             lastState = state
-            print('+{}s: state is now {}'.format(t / poll_freq, state))
-            t = 0
+            print('+{}s: state is now {}'.format(pulseperiod, state))
+            if state == 1:
+                now = time.time()
+                pulseperiod = now - t
+                t = now
+                print('set output')
+                GPIO.output(ochannel, 1)
+
+        gate = pulseperiod - pulsewidth
+        if (time.time() - t > gate) and GPIO.input(ochannel):
+            print('+{}s: reset output'.format(t / poll_freq))
+            GPIO.output(ochannel, 0)
+
         time.sleep(1/poll_freq)
-        t += 1
+
+    GPIO.cleanup()
 
 if __name__ == "__main__":
     main()
