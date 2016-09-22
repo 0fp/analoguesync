bb1f856e3e68cd05906b7f41591dfe2410a0d980
0fp <0fp@altlast.tk>
2016-09-22 15:34:59 +0000

    read clock state from gpio

--- analoguesync.py
+++ analoguesync.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python
 #coding: utf8
 
+import RPi.GPIO as GPIO
+import time
+
 def main():
-    pass
+    channel = 22
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    lastState = 0
+    t = 0
+    while True:
+        GPIO
+        state = GPIO.input(channel)
+        if state != lastState:
+            lastState = state
+            print('{} state is now {}'.format(t, state))
+        time.sleep(0.001)
+        t += 1
 
 if __name__ == "__main__":
     main()
