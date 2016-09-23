0f211dcf3a29322a92dcbec1db4130fce7720cb7
0fp <ich@altlast.tk>
2016-09-23 11:02:27 +0000

    fix bug with cycle_length change

--- analoguesync.py
+++ analoguesync.py
@@ -18,34 +18,45 @@ class LFO():
         self._rise = rise
         self._fall = fall
 
-        self.t_fall = Timer(0, fall)
+        self.t_rise = Timer(0, self.rise)
+        self.t_fall = Timer(0, self.fall)
 
     def __del__(self):
         self.t_fall.cancel()
 
     def set_cycle(self, cycle_length):
+
+        self.cycle_length = cycle_length
+
+        # always reset for new cycle_length
+        self.t_rise.cancel()
+        self.t_fall.cancel()
+
         if self.multiplier == 0:
             return
 
         # start of cycle
+        self.rise()
+
+    def rise(self):
         if self.pulse == 0:
-            self.rise()
+            self._rise()
 
         # register estimated end of cycle
-        t = (self.multiplier - self.pulse) * cycle_length - self.min_pw
+        t = (self.multiplier - self.pulse) * self.cycle_length - self.min_pw
         self.t_fall.cancel()
         self.t_fall = Timer(t, self.fall)
         self.t_fall.start()
 
-        self.pulse += 1
-
-    def rise(self):
-        self._rise()
+        self.pulse = (self.pulse + 1) % self.multiplier
 
     def fall(self):
         self._fall()
-        if self.pulse == self.multiplier:
-            self.pulse = 0
+
+        # register next start of cycle
+        self.t_rise.cancel()
+        self.t_rise = Timer(self.min_pw, self.rise)
+        self.t_rise.start()
 
 def mkclick():
     measure = 0
